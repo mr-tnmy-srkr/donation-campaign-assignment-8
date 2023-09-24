67a0bb5fff401cb539d10631f7fc6e5c469d97e2
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-25 01:12:11 +0530

    navbar added

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+     {
+        path: "/",
+        element: <Home></Home>,
+      }, 
+    ],
+  },
+]);
 
-
-  export default router
+export default router;
